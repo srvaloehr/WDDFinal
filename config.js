7b2bbf7a6668441d4f7f27528cd1b4788bae411d
stephen <your@email.com>
2026-06-15 19:11:52 -0700

    updated api keys

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const TMDB_API_KEY = "YOUR_TMDB_API_KEY_HERE";
-export const OMDB_API_KEY = "YOUR_OMDB_API_KEY_HERE";
+export const TMDB_API_KEY = "5884f5a72a03b6c3356a0949db6feaf9";
+export const OMDB_API_KEY = "8a3c484c";
  
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
