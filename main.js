cef88c318c43b35713a39d1d1bc296467044a31b
stephen <your@email.com>
2026-06-15 19:19:19 -0700

    test

--- main.js
+++ main.js
@@ -386,7 +386,7 @@ function renderSnack(snack) {
 // Pick for us
 randomPickBtn.addEventListener("click", function () {
   if (lastMovies.length === 0) return;
-
+console.log("lastMovies:", lastMovies, "card found:", card);
   var randomIndex = Math.floor(Math.random() * lastMovies.length);
   var chosenMovie = lastMovies[randomIndex];
   var card = document.querySelector('[data-movie-id="' + chosenMovie.id + '"]');
